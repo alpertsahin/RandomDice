929350ec799b85d368e5ce3c4248f5eb693eb2cb
alpertsahin <alpersahin206@gmail.com>
2023-04-19 19:42:02 +0300

    background changed

--- diceRolling/ViewController.swift
+++ diceRolling/ViewController.swift
@@ -11,8 +11,9 @@ class ViewController: UIViewController {
     
     let mainLogo = UIImage(named: "mainLogo")
     let mainLogoIV = UIImageView()
-    let randomButton = UIButton()
-    
+   
+    let neonLogo = UIImage(named: "neonButton2")
+    let randomButton = UIButton(type: .custom)
     
     var leftDice = UIImage(named: "dice1")
     var leftDiceIV = UIImageView()
@@ -35,7 +36,7 @@ class ViewController: UIViewController {
     func configure(){
         
         
-        let background = UIImage(named: "newbackground")!
+        let background = UIImage(named: "mainLogo2")!
         let backgroundImageView = UIImageView(frame: UIScreen.main.bounds)
         backgroundImageView.image = background
         view.addSubview(backgroundImageView)
@@ -56,29 +57,29 @@ class ViewController: UIViewController {
         view.addSubview(rightDiceIV)
         
         // Yatay eksen için constraint (ekranın sol yarısı)
-        leftDiceIV.centerXAnchor.constraint(equalTo: view.leadingAnchor, constant: view.bounds.width/4).isActive = true
+        leftDiceIV.centerXAnchor.constraint(equalTo: view.leadingAnchor, constant: view.bounds.width/3.5).isActive = true
 
         // Dikey eksen için constraint (ekranın ortası)
         leftDiceIV.centerYAnchor.constraint(equalTo: view.centerYAnchor,constant: 50).isActive = true
 
         // Genişlik constraint'i
-        leftDiceIV.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        leftDiceIV.widthAnchor.constraint(equalToConstant: 130).isActive = true
 
         // Yükseklik constraint'i
-        leftDiceIV.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        leftDiceIV.heightAnchor.constraint(equalToConstant: 130).isActive = true
         
         
         // Yatay eksen için constraint (ekranın sağ yarısı)
-        rightDiceIV.centerXAnchor.constraint(equalTo: view.trailingAnchor, constant: -view.bounds.width/4).isActive = true
+        rightDiceIV.centerXAnchor.constraint(equalTo: view.trailingAnchor, constant: -view.bounds.width/3.5).isActive = true
 
         // Dikey eksen için constraint (ekranın ortası)
         rightDiceIV.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50).isActive = true
 
         // Genişlik constraint'i
-        rightDiceIV.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        rightDiceIV.widthAnchor.constraint(equalToConstant: 130).isActive = true
 
         // Yükseklik constraint'i
-        rightDiceIV.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        rightDiceIV.heightAnchor.constraint(equalToConstant: 130).isActive = true
         
         
         
@@ -86,9 +87,11 @@ class ViewController: UIViewController {
         randomButton.setTitle("ZAR AT", for: UIControl.State.normal)
         randomButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(randomButton)
-        randomButton.setTitleColor(.white, for: UIControl.State.normal)
-        randomButton.backgroundColor = .black
-        randomButton.layer.cornerRadius = 20
+        randomButton.setTitleColor(.magenta, for: .normal)
+        randomButton.titleLabel?.font = UIFont.systemFont(ofSize: 22)
+        randomButton.setBackgroundImage(neonLogo, for: .normal)
+        randomButton.layer.cornerRadius = 100
+        
         
         // Yatay eksen için constraint (x koordinatı ortası)
         randomButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
@@ -123,49 +126,87 @@ class ViewController: UIViewController {
     
     @objc func zarAtButton() {
             
+        
         let firstDice = Int(arc4random_uniform(6))
         let secondDice = Int(arc4random_uniform(6))
         randomButton.setTitle("BEKLE...", for: .normal)
         randomButton.isEnabled = false
-        let duration = Double(arc4random_uniform(2) + 2) // 3-6 saniye arasında rastgele bir süre belirle
-
+        let duration = Double(Double(arc4random_uniform(UInt32(0.05))) + 0.05)
         DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
-            self.randomButton.setTitle("Zar At", for: .normal)
             self.leftDiceIV.image = self.items[firstDice]
             self.rightDiceIV.image = self.items[secondDice]
-            self.randomButton.isEnabled = true
+            
         }
         
-        print(firstDice)
-        print(secondDice)
-    }
-    
-    /* @objc func zarAtButton() {
+        let firstDice1 = Int(arc4random_uniform(6))
+        let secondDice1 = Int(arc4random_uniform(6))
+        randomButton.setTitle("BEKLE...", for: .normal)
+        randomButton.isEnabled = false
+
+        let duration1 = Double(Double(arc4random_uniform(UInt32(0.2))) + 0.2) //
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration1) {
+            self.leftDiceIV.image = self.items[firstDice1]
+            self.rightDiceIV.image = self.items[secondDice1]
+            
+        }
         
-        let firstDice = Int(arc4random_uniform(6))
-        let secondDice = Int(arc4random_uniform(6))
+        let firstDice2 = Int(arc4random_uniform(6))
+        let secondDice2 = Int(arc4random_uniform(6))
+        randomButton.setTitle("BEKLE...", for: .normal)
+        randomButton.isEnabled = false
+
+        let duration2 = Double(Double(arc4random_uniform(UInt32(0.4))) + 0.4)
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration2) {
+            self.leftDiceIV.image = self.items[firstDice2]
+            self.rightDiceIV.image = self.items[secondDice2]
+            
+        }
         
+        let firstDice3 = Int(arc4random_uniform(6))
+        let secondDice3 = Int(arc4random_uniform(6))
+        randomButton.setTitle("BEKLE...", for: .normal)
         randomButton.isEnabled = false
         
-        let duration = Double(arc4random_uniform(4) + 3) // 3-6 saniye arasında rastgele bir süre belirle
+        let duration3 = Double(Double(arc4random_uniform(UInt32(0.8))) + 0.8)
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration3) {
+  
+            self.leftDiceIV.image = self.items[firstDice3]
+            self.rightDiceIV.image = self.items[secondDice3]
+           
+           
+        }
+        
+        
+        let firstDice4 = Int(arc4random_uniform(6))
+        let secondDice4 = Int(arc4random_uniform(6))
+        randomButton.setTitle("BEKLE...", for: .normal)
+        randomButton.isEnabled = false
 
-                DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
-                    self.randomButton.isEnabled = true // Süre dolduğunda butonu etkinleştir
-                }
+        let duration4 = Double(Double(arc4random_uniform(UInt32(1.3))) + 1.3)
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration4) {
+            self.leftDiceIV.image = self.items[firstDice4]
+            self.rightDiceIV.image = self.items[secondDice4]
+            
+        }
         
-        
-   /*    let duration = Double(arc4random_uniform(4) + 3) // 3-6 saniye arası rastgele bir süre belirliyoruz
-        DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
-            self.leftDiceIV.image = self.items[firstDice]
-            self.rightDiceIV.image = self.items[secondDice]
+        let firstDice5 = Int(arc4random_uniform(6))
+        let secondDice5 = Int(arc4random_uniform(6))
+        randomButton.setTitle("BEKLE...", for: .normal)
+        randomButton.isEnabled = false
+
+        let duration5 = Double(Double(arc4random_uniform(UInt32(1.7))) + 1.7)
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration5) {
+            self.randomButton.setTitle("ZAR AT", for: .normal)
+            self.leftDiceIV.image = self.items[firstDice5]
+            self.rightDiceIV.image = self.items[secondDice5]
             self.randomButton.isEnabled = true
             
-        } */
-    
-       
-        print(firstDice)
-        print(secondDice)
-    } */
-    
+        }
+        print(firstDice5)
+        print(secondDice5)
+    }
+
+
 }
 
